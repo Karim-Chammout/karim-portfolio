110c7c74ad2b6b6f999a92a58791e0e4dcb6481e
Karim <61624676+Karim-Chammout@users.noreply.github.com>
2022-10-29 18:29:01 +0300

    Stupid Vercel deployment error

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,6 @@ export { default as Container } from './Container';
 export { default as SocialMedia } from './socialMedia';
 export { default as Navbar } from './Navbar';
 export { default as Seo } from './Seo';
-export { default as Footer } from './Footer';
-export { default as TopButton } from './TopButton';
+export { default as Footer } from './footer';
+export { default as TopButton } from './topButton';
 export { default as ScrollToTop } from './ScrollToTop';
